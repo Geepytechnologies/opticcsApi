029c1f9f59945c1bab18702a9a85e12fd481c1bf
geepy technologies <geepytechnologies@gmail.com>
2023-07-10 11:59:59 +0100

    new

--- config/db.js
+++ config/db.js
@@ -1,11 +1,21 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+
+  console.log("Connected to MySQL!");
+  // You can start executing queries or performing operations on the database here
 });
 
 module.exports = db;
