afd96e92a78f70ce3781e246445c2c568b2bf9f2
geepy technologies <geepytechnologies@gmail.com>
2023-08-29 16:50:58 +0100

    new

--- controllers/admin/national/auth.js
+++ controllers/admin/national/auth.js
@@ -2,6 +2,84 @@ const db = require("../../../config/db");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// const signin = async (req, res, next) => {
+//   const connection = await db.getConnection();
+//   const { userid } = req.body;
+
+//   const existinguserid = async () => {
+//     const q = `SELECT * FROM nationaladmin WHERE userid = ?`;
+//     const result = await connection.execute(q, [userid]);
+//     return result[0];
+//   };
+//   const createRefresh = async (refreshToken) => {
+//     const q = `UPDATE nationaladmin
+//         SET refreshtoken = ?
+//         WHERE userid = ?`;
+//     const result = await connection.execute(q, [refreshToken, userid]);
+//     return result[0];
+//   };
+
+//   try {
+//     const user = await existinguserid();
+//     if (!user.length)
+//       return res
+//         .status(404)
+//         .json({ statusCode: "404", message: "User not found" });
+//     const isMatched = bcrypt.compareSync(req.body.password, user[0].password);
+//     if (!isMatched)
+//       return res
+//         .status(400)
+//         .json({ statusCode: "400", message: "wrong credentials" });
+
+//     //access Token
+//     const accessToken = jwt.sign(
+//       { id: user[0].id },
+//       process.env.ACCESS_SECRET,
+//       {
+//         expiresIn: "5m",
+//       }
+//     );
+
+//     //refresh Token
+//     const refreshToken = jwt.sign(
+//       { id: user[0].id },
+//       process.env.REFRESH_SECRET,
+//       {
+//         expiresIn: "30m",
+//       }
+//     );
+
+//     //save refresh token to the user model
+//     await createRefresh(refreshToken);
+//     const newuser = await existinguserid();
+
+//     // Creates Secure Cookie with refresh token
+//     res.cookie("nationaltoken", refreshToken, {
+//       httpOnly: false,
+//       secure: true,
+//       sameSite: "None",
+//       maxAge: 10 * 24 * 60 * 60 * 1000,
+//     });
+
+//     const { password, refreshtoken, ...others } = newuser[0];
+
+//     res.status(200).json({
+//       statusCode: "200",
+//       message: "successful",
+//       result: { others: others, accessToken },
+//     });
+//   } catch (err) {
+//     connection.rollback();
+//     res
+//       .status(500)
+//       .json({ statusCode: "500", message: "Error signing in", error: err });
+//     next(err);
+//   } finally {
+//     if (connection) {
+//       connection.release();
+//     }
+//   }
+// };
 const signin = async (req, res, next) => {
   const connection = await db.getConnection();
   const { userid } = req.body;
@@ -11,12 +89,14 @@ const signin = async (req, res, next) => {
     const result = await connection.execute(q, [userid]);
     return result[0];
   };
+
   const createRefresh = async (refreshToken) => {
-    const q = `UPDATE nationaladmin
-        SET refreshtoken = ?
-        WHERE userid = ?`;
-    const result = await connection.execute(q, [refreshToken, userid]);
-    return result[0];
+    const q = `UPDATE nationaladmin SET refreshtoken = ? WHERE userid = ?`;
+    try {
+      await connection.execute(q, [refreshToken, userid]);
+    } catch (err) {
+      throw err; // Handle error appropriately
+    }
   };
 
   try {
@@ -25,13 +105,14 @@ const signin = async (req, res, next) => {
       return res
         .status(404)
         .json({ statusCode: "404", message: "User not found" });
+
     const isMatched = bcrypt.compareSync(req.body.password, user[0].password);
     if (!isMatched)
       return res
         .status(400)
         .json({ statusCode: "400", message: "wrong credentials" });
 
-    //access Token
+    // Access Token
     const accessToken = jwt.sign(
       { id: user[0].id },
       process.env.ACCESS_SECRET,
@@ -40,7 +121,7 @@ const signin = async (req, res, next) => {
       }
     );
 
-    //refresh Token
+    // Refresh Token
     const refreshToken = jwt.sign(
       { id: user[0].id },
       process.env.REFRESH_SECRET,
@@ -49,9 +130,8 @@ const signin = async (req, res, next) => {
       }
     );
 
-    //save refresh token to the user model
+    // Save refresh token to the user model
     await createRefresh(refreshToken);
-    const newuser = await existinguserid();
 
     // Creates Secure Cookie with refresh token
     res.cookie("nationaltoken", refreshToken, {
@@ -61,7 +141,7 @@ const signin = async (req, res, next) => {
       maxAge: 10 * 24 * 60 * 60 * 1000,
     });
 
-    const { password, refreshtoken, ...others } = newuser[0];
+    const { password, refreshtoken, ...others } = user[0];
 
     res.status(200).json({
       statusCode: "200",
@@ -69,10 +149,11 @@ const signin = async (req, res, next) => {
       result: { others: others, accessToken },
     });
   } catch (err) {
-    connection.rollback();
-    res
-      .status(500)
-      .json({ statusCode: "500", message: "Error signing in", error: err });
+    res.status(500).json({
+      statusCode: "500",
+      message: "Error signing in",
+      error: err.message || "Internal server error",
+    });
     next(err);
   } finally {
     if (connection) {
@@ -80,6 +161,7 @@ const signin = async (req, res, next) => {
     }
   }
 };
+
 const resetpassword = async (req, res, next) => {
   const connection = await db.getConnection();
   const { oldpassword, newpassword } = req.body;
@@ -135,6 +217,40 @@ const resetpassword = async (req, res, next) => {
   }
 };
 
+// const handleRefreshToken = async (req, res) => {
+//   const connection = await db.getConnection();
+//   const existingRefresh = async (refreshToken) => {
+//     const q = `SELECT * FROM nationaladmin WHERE refreshToken = ?`;
+//     const result = await connection.execute(q, [refreshToken]);
+//     return result[0];
+//   };
+//   const cookies = req.cookies;
+//   if (!cookies?.nationaltoken) return res.sendStatus(401);
+//   const refreshToken = cookies.nationaltoken;
+//   try {
+//     const foundUser = await existingRefresh(refreshToken);
+//     if (!foundUser.length) {
+//       return res.status(403).json("User not Found");
+//     }
+//     // evaluate jwt
+//     jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, user) => {
+//       if (err || foundUser[0]?.id !== user.id) return res.sendStatus(403);
+//       const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_SECRET, {
+//         expiresIn: "5m",
+//       });
+//       const { password, refreshtoken, ...others } = foundUser[0];
+
+//       res.json({ accessToken, others: others });
+//       // connection.release();
+//     });
+//   } catch (err) {
+//     res.status(500).json({ message: "error refreshing token", error: err });
+//   } finally {
+//     if (connection) {
+//       connection.release();
+//     }
+//   }
+// };
 const handleRefreshToken = async (req, res) => {
   const connection = await db.getConnection();
   const existingRefresh = async (refreshToken) => {
@@ -142,24 +258,33 @@ const handleRefreshToken = async (req, res) => {
     const result = await connection.execute(q, [refreshToken]);
     return result[0];
   };
+
   const cookies = req.cookies;
+  console.log({ cookiesfromrefresh: cookies });
   if (!cookies?.nationaltoken) return res.sendStatus(401);
   const refreshToken = cookies.nationaltoken;
+
   try {
     const foundUser = await existingRefresh(refreshToken);
     if (!foundUser.length) {
       return res.status(403).json("User not Found");
     }
-    // evaluate jwt
-    jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, user) => {
-      if (err || foundUser[0]?.id !== user.id) return res.sendStatus(403);
-      const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_SECRET, {
-        expiresIn: "5m",
-      });
-      const { password, refreshtoken, ...others } = foundUser[0];
 
-      res.json({ accessToken, others: others });
-      connection.release();
+    // Verify JWT
+    jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, user) => {
+      if (err || foundUser[0]?.id !== user.id) {
+        return res.sendStatus(403);
+      } else {
+        const accessToken = jwt.sign(
+          { id: user.id },
+          process.env.ACCESS_SECRET,
+          {
+            expiresIn: "5m",
+          }
+        );
+        const { password, refreshToken, ...others } = foundUser[0];
+        return res.json({ accessToken, others });
+      }
     });
   } catch (err) {
     res.status(500).json({ message: "error refreshing token", error: err });
