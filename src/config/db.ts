b0b7b3532054b371fef3e5416cdc93468d77dbb6
Geepy <geepytechnologies@gmail.com>
2024-02-18 10:13:48 +0100

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,9 +8,9 @@ const db: Pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD || "",
   database: process.env.MYSQLDATABASE || "your_database_name",
   waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  connectionLimit: 30,
+  maxIdle: 30, // max idle connections, the default value is the same as `connectionLimit`
+  //idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
