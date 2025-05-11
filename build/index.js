e7912c5aa0d46a8749b10231c906619ccebb2e61
Geepytechnologies <geepytechnologies@gmail.com>
2025-05-11 23:33:17 +0100

    build added

--- build/index.js
+++ build/index.js
@@ -80,11 +80,8 @@ const path = __importStar(require("path"));
 const schedulereminder_service_1 = require("./services/schedulereminder.service");
 const missedschedule_service_1 = require("./services/missedschedule.service");
 const swagger_1 = __importDefault(require("./config/swagger"));
-<<<<<<< HEAD
-//require("./utils/settlementSeeder");
-//require("./utils/healthfacilitySeeder");
-
-
+// import "./utils/settlementSeeder";
+// import "./utils/healthfacilitySeeder";
 schedulereminder_service_1.reminderSMS.start();
 missedschedule_service_1.missedscheduleSMS.start();
 const adminOptions = {
